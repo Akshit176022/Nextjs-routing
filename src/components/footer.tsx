fd04cca03afa0cbf429295a9f2e0727f4e8b3373
akshit Pathania <akshitpathania75@gmail.com>
2024-03-19 17:22:45 +0530

    Navbar

---
+++ src/components/footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div className=" text-gray-400 py-12 mt-10">
+<div className='max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
+    <div>Abouts us
+        <div className='p-3 items-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore nobis quidem blanditiis culpa excepturi repellat quo possimus sequi perspiciatis temporibus repellendus rerum neque ea et reprehenderit dignissimos molestiae quam, voluptate eum non. Non perferendis, harum beatae id accusamus voluptatem.</div> </div>
+    <div>Important Links </div>
+    <div>Follow us 
+    <div className='flex flex-row gap-4'>
+        <div>Facebook</div>
+        <div>Twitter</div>
+        <div>Instagram</div>
+    </div>
+    </div>
+
+    <div>contact us </div>
+</div>
+    </div>
+  )
+}
+
+export default footer
+
+
+
