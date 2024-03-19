deb57f9ba65530b307993972a956f102b2bc7d8f
akshit Pathania <akshitpathania75@gmail.com>
2024-03-20 01:35:43 +0530

    rules page added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,13 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 w-full inset-x-0 max-w-2xl mx-auto z-50 ", className)}
+      className={cn("fixed top-0 w-full inset-x-0 max-w-2xl mx-auto z-50 ", className)}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Leaderboard">
 
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Login">
+        <MenuItem setActive={setActive} active={active} item="Sign Up">
 
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Rules">
